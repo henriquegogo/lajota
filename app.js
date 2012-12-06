2449e4c49da3cc7c0d96155b2cc3f1365b8877f0
Henrique Gogó <henriquegogo@gmail.com>
2012-12-06 01:18:49 -0300

    Adding jqueyr ui for dialogs and working on style of grid and product modal

--- app.js
+++ app.js
@@ -35,6 +35,52 @@
 })();
 
 (function() {
+    String.prototype.toUrl = function() {
+        return this.toLowerCase()
+                   .replace(/^\s+|\s+$/g, "")
+                   .replace(/[_|\s]+/g, "-")
+                   .replace(/[^a-z0-9-]+/g, "")
+                   .replace(/[-]+/g, "-")
+                   .replace(/^-+|-+$/g, "");
+    };
+
+    String.prototype.toFloat = function() {
+        return parseFloat(this);
+    };
+
+    if (!Array.prototype.filter)
+    {
+      Array.prototype.filter = function(fun /*, thisp */)
+      {
+        "use strict";
+     
+        if (this == null)
+          throw new TypeError();
+     
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun != "function")
+          throw new TypeError();
+     
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+          if (i in t)
+          {
+            var val = t[i]; // in case fun mutates this
+            if (fun.call(thisp, val, i, t))
+              res.push(val);
+          }
+        }
+     
+        return res;
+      };
+    }
+})();
+
+    
+(function() {
     var router = function(routes) {
         $.extend(app.routes, routes);
         routes = app.routes;
@@ -104,19 +150,33 @@
         });
     };
 
-    var app = {
+    this.app = {
         router: router,
         routes: {},
         render: render,
         container: "body"
     };
-
-    window.app = app;
 })();
 
 (function() {
-    app.container = "#content";
+    var data = {
+        products: [
+            { id: 1, title: "Toalha de mesa em renda de bilro", description: "Linda toalha toda trabalhada no mais perfeito detalhe", price: "150,00" },
+            { id: 2, title: "Saída de praia com detalhes bordados a ouro", description: "Linda toalha", price: "400,00" },
+            { id: 3, title: "Centro de mesa simples", description: "Linda toalha", price: "30,00" },
+            { id: 4, title: "Toalha de mesa em renda de bilro", description: "Linda toalha toda trabalhada no mais perfeito detalhe", price: "150,00" },
+            { id: 5, title: "Saída de praia com detalhes bordados a ouro", description: "Linda toalha", price: "400,00" },
+            { id: 6, title: "Centro de mesa simples", description: "Linda toalha", price: "30,00" },
+            { id: 7, title: "Toalha de mesa em renda de bilro", description: "Linda toalha toda trabalhada no mais perfeito detalhe", price: "150,00" },
+            { id: 8, title: "Saída de praia com detalhes bordados a ouro", description: "Linda toalha", price: "400,00" },
+            { id: 9, title: "Centro de mesa simples", description: "Linda toalha", price: "30,00" },
+            { id: 10, title: "Toalha de mesa em renda de bilro", description: "Linda toalha toda trabalhada no mais perfeito detalhe", price: "150,00" },
+            { id: 11, title: "Saída de praia com detalhes bordados a ouro", description: "Linda toalha", price: "400,00" },
+            { id: 12, title: "Centro de mesa simples", description: "Linda toalha", price: "30,00" }
+        ]
+    };
 
+    app.container = "#content";
     app.router({
         before: function() {
             $(document).ajaxStart(function(){
@@ -132,25 +192,21 @@
         },
         
         "#/": function() {
-            var data = { products: ["Rechilieu", "Bilro", "Croché"] };
-
-            app.render("internal", data);
+            app.render("template_grid", data);
         },
 
-        "#/user/:id": function(id) {
-            console.log("Opening", id);
-        },
+        "#/item/:id/:title": function(id) {
+            var item = data.products.filter(function(i) { return i.id == id; })[0];
 
-        "#/user/:id/edit": function(id) {
-            console.log("Editing", id);
-        },
+            var openDialog = function(content) {
+                $(content).dialog({
+                    modal: true,
+                    width: 900,
+                    close: function() { history.back() }
+                });
+            };
 
-        "#/user/:id/:curriculum": function(id, cv) {
-            console.log("Editing " + id + " with cv " + cv);
-        },
-
-        "#/help": function() {
-            console.log('help');
+            app.render("template_item", item, openDialog);
         }
     });
 })();
