07bb9d8b422ca83f82a1b34e3641505a28c2ca79
Henrique Gogó <henriquegogo@gmail.com>
2013-10-09 01:06:43 -0300

    Working on social example, creating sessions and pages

--- examples/social/app.js
+++ examples/social/app.js
@@ -12,5 +12,36 @@
         "#/help": function() {
             app.render("template_help");
         },
+        
+        "#/bikes": function() {
+            var data = {
+                bikes: [
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 12, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Princesa' - Monark City" },
+                    { id: 13, thumbPath: "images/bike-list-item-thumb.jpg", info: "'Azulzinha' - Houston Enterprise" }
+                ]
+            };
+            
+            app.render("template_bikes", data);
+        },
+        
+        "#/users": function() {
+            app.render("template_users");
+        },
     });
 })();
